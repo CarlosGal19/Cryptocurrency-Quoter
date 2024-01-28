9a6150fb90cdc03c221a4d4e0435c682352307d1
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-01-28 14:50:58 -0600

    feat: form is filled at the beginning calling the API

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
-const currency = document.querySelector('#moneda');
-const cryptoCurrency = document.querySelector('#criptomonedas');
 const form=document.querySelector('#formulario')
+const cryptoCurrence=document.querySelector('#criptomonedas');
+const currence=document.querySelector('#moneda');
+
+document.addEventListener('DOMContentLoaded', callAPI);
+
+function callAPI(){
+    fetch('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=MXN')
+        .then(response=>response.json())
+        .then(data => getCryptoCurrencies(data.Data))
+        .then(cryptoCurrencies => fillForm(cryptoCurrencies))
+}
+
+const getCryptoCurrencies = ( cryptoCurrencies ) => new Promise( resolve =>{
+    resolve(cryptoCurrencies);
+})
+
+function fillForm(dataCryptos){
+    dataCryptos.forEach(Coin => {
+        const {FullName, Name} = Coin.CoinInfo;
+        const option=document.createElement('option');
+        option.textContent=FullName;
+        option.value=Name;
+        cryptoCurrence.appendChild(option);
+    });
+}
